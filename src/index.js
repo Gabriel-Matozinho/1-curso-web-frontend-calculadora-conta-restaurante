d1e24aeb6a4ccb0263fa8caf33ffcc183f3bb970
Gabriel Matozinho <gabriel.intec@hotmail.com>
2025-04-28 21:09:38 -0300

    resoução calculadora

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ const metodoPagamento = readline.question('Qual é o método de pagamento (PIX, 
 
 
 //Façã sua lógica para aplicar o desconto apenas para PIX OU DINHEIRO
+let desconto = valorTotal;
+let porpessoa = numeroPessoas;
+if (metodoPagamento === "PIX" || metodoPagamento === "dinheiro") {
+    desconto -= desconto * (10 / 100)
+    porpessoa = desconto / porpessoa
+}
 
 
 // Exibindo os resultados
+console.log(`O valor fica R$ ${porpessoa.toFixed(2)}`);
