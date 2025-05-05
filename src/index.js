33865ca33dfbf2efd0c62ae5a50346cb8b92f0b6
Gabriel Matozinho <gabriel.intec@hotmail.com>
2025-05-05 19:19:44 -0300

    Correcao da lógica

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 const readline = require('readline-sync');
 
 // Capturando o número de pessoas na mesa
-const numeroPessoas = readline.questionInt('Digite o número de pessoas na mesa: ');
+const numeroPessoas = readline.questionInt('Digite o numero de pessoas na mesa: ');
 
 // Capturando o valor total da conta
 const valorTotal = readline.questionFloat('Digite o valor total da conta: ');
 
 // Capturando o método de pagamento
-const metodoPagamento = readline.question('Qual é o método de pagamento (PIX, dinheiro ou cartão)? ');
+const metodoPagamento = readline.question('Qual é o metodo de pagamento (PIX, dinheiro ou cartao)? ');
 
 
 //Façã sua lógica para aplicar o desconto apenas para PIX OU DINHEIRO
@@ -16,6 +16,8 @@ let desconto = valorTotal;
 let porpessoa = numeroPessoas;
 if (metodoPagamento === "PIX" || metodoPagamento === "dinheiro") {
     desconto -= desconto * (10 / 100)
+}
+else{
     porpessoa = desconto / porpessoa
 }
 
